0e529b6c666b4de79f6a18fbbdabb6dafeb078e5
Holly Johnson <hollyjnz@gmail.com>
2018-11-01 11:14:29 +1300

    Thumbnail functionality

--- image-gallery/js/main.js
+++ image-gallery/js/main.js
@@ -1,3 +1,13 @@
+// HTML Elements
+// var thumbnails = $('.thumbnails');
+// var thumbnailImgs = $('.thumbnails img');
+// var imageContainer = $('.image-container');
+// var firstThumbnail = $('.thumbnails img:first-child');
+// var selectedThumbnail = $('.outline');
+// var mainImg = $('.main-image');
+// var leftArrow = $('.fa-chevron-left');
+// var rightArrow = $('.fa-chevron-right');
+
 // Image array
 var imgArr = [
     'img/square100.png',
@@ -5,29 +15,70 @@ var imgArr = [
     'img/square300.png'
 ];
 
+// Index counter to keep track of which thumbnail is selected
+var selected = 0;
+
+// Loads array of images and sets attributes
 function loadImgArray(imgArr) {
     // Add thumbnails
-    for(var i = 0; i < imgArr.length; i++) {
+    for (var i = 0; i < imgArr.length; i++) {
         var img = new Image();
         img.src = imgArr[i];
-       $('.thumbnails').append(img);
+        $('.thumbnails').append(img);
     }
-    // Add main image
+    var count = 0, thumbnails = $('.thumbnails img');
+    thumbnails.each(function () {
+        var dataId = $(this).attr('data-id');
+        if (typeof dataId === typeof undefined) {
+            $(this).attr('data-id', count++);
+        };
+    });
+    // Load main image
     var mainImg = new Image();
     mainImg.src = imgArr[0];
     mainImg.className = "main-image";
     $('.image-container').prepend(mainImg);
+    $('.thumbnails img:first-child').toggleClass('outline');
 };
 
+// Update selected thumbnail
+function updateSelected(selected) {
+    $('.outline').toggleClass('outline');
+    // Get the element with data-id === selected
+    var updatedImg = $(`img[data-id=${selected}]`);
+    updatedImg.toggleClass('outline');
+    var newSrc = updatedImg.attr('src');
+    $('.main-image').attr('src', newSrc);
+    return;
+}
+
+// When window is loaded, loads images and sets event listeners
 $(document).ready(function () {
     loadImgArray(imgArr);
     $('.thumbnails img').click(function () {
-        if(!$(this).hasClass('outline')) {
-            $('.outline').toggleClass('outline');
-            $(this).toggleClass('outline');
-        }
-        var oldSrc = $('.main-image').attr('src');
+        $('.outline').toggleClass('outline');
+        $(this).toggleClass('outline');
         var newSrc = $(this).attr('src');
         $('.main-image').attr('src', newSrc);
+    });
+    $('.fa-chevron-left').click(function () {
+        if (selected == 0) {
+            selected = imgArr.length - 1;
+            updateSelected(selected);
+            return;
+        }
+        selected--;
+        updateSelected(selected);
+        return;
+    });
+    $('.fa-chevron-right').click(function () {
+        if (selected == imgArr.length - 1) {
+            selected = 0;
+            updateSelected(selected);
+            return;
+        }
+        selected++;
+        updateSelected(selected);
+        return;
     });
 });
